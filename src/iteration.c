e829d9a56104d3c1449a30b907e6410bc463aa1b
SeidM27 <seidmamuti1313@gmail.com>
2026-05-10 12:56:38 +0200

    Update iteration.c
    seid mamuti assignment 2

--- src/iteration.c
+++ src/iteration.c
@@ -23,3 +23,22 @@ Rules:
 - No recursion allowed.
 - Output must match exactly.
 */
+#include <cs50.h>
+#include <stdio.h>
+int main(void)
+{
+int n;
+do
+{
+n = get_int("Input: ");
+}
+while (n < 1);
+for (int i = 1; i <= n; i++)
+{
+for (int j = 0; j < i; j++)
+{
+printf("#");
+}
+printf("\n");
+}
+}
